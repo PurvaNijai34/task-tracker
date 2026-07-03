24b98e3043cbcc93c41f15d34b7b05ca96e49705
PurvaNijai34 <purvanijai123@gmail.com>
2026-07-03 11:08:46 +0530

    Implement task controller

---
+++ server/src/controllers/task.controller.js
@@ -0,0 +1,65 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiResponse from "../utils/ApiResponse.js";
+
+import {
+  createTask,
+  getAllTasks,
+  getTaskById,
+  updateTask,
+  deleteTask,
+} from "../services/task.service.js";
+
+
+export const create = asyncHandler(async (req, res) => {
+  const task = await createTask({
+    ...req.body,
+    userId: req.user._id,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, "Task created successfully", task));
+});
+
+
+export const getAll = asyncHandler(async (req, res) => {
+  const tasks = await getAllTasks(req.user._id);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Tasks fetched successfully", tasks));
+});
+
+
+export const getOne = asyncHandler(async (req, res) => {
+  const task = await getTaskById(
+    req.params.id,
+    req.user._id
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Task fetched successfully", task));
+});
+
+
+export const update = asyncHandler(async (req, res) => {
+  const task = await updateTask(
+    req.params.id,
+    req.user._id,
+    req.body
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Task updated successfully", task));
+});
+
+
+export const remove = asyncHandler(async (req, res) => {
+  await deleteTask(req.params.id, req.user._id);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Task deleted successfully"));
+});
